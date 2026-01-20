cf24e3756824ee5ea0effbc2f97c9da77407c5c0
koyu <me@koyu.space>
2026-01-20 01:01:27 +0000

    feat: Update Windows build script to embed manifest and link against Comctl32

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,21 +1,34 @@
 fn main() {
-    // Allow cross-compilation: only check target OS, not host OS
     #[cfg(target_os = "windows")]
     {
-        // Set Windows subsystem to windows for GUI application
-        println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
-
-        // Embed manifest for UAC elevation (administrator privileges)
-        // Only works when using native Windows compilation or with winresource feature
-        #[cfg(feature = "windows-resources")]
-        {
-            let mut res = winresource::WindowsResource::new();
-            res.set_manifest_file("manifest.xml");
-            if let Err(e) = res.compile() {
-                eprintln!("Warning: Failed to compile Windows resources: {}", e);
-            }
-        }
+        // Link against Comctl32 for TaskDialogIndirect
+        println!("cargo:rustc-link-lib=Comctl32");
     }
 
-    tauri_build::build()
+    let mut windows = tauri_build::WindowsAttributes::new();
+    windows = windows.app_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+        type="win32"
+        name="Microsoft.Windows.Common-Controls"
+        version="6.0.0.0"
+        processorArchitecture="*"
+        publicKeyToken="6595b64144ccf1df"
+        language="*"
+      />
+    </dependentAssembly>
+  </dependency>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#);
+    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attrs).expect("failed to run build script");
 }
